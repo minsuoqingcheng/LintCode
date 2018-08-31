eed911b71b4e7c8149f9c021619e766b2fe2be8c
chengliu <1397004810@qq.com>
2018-08-31 20:23:11 +0800

    [question 21] merge两个有序的链表

---
+++ src/main/LeetCode/question21/MergeTwoLists.java
@@ -0,0 +1,62 @@
+package main.LeetCode.question21;
+
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode res = new ListNode(0);
+        ListNode current = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = new ListNode(l1.val);
+                current = current.next;
+                l1 = l1.next;
+            } else {
+                current.next = new ListNode(l2.val);
+                current = current.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) {
+            current.next = l1;
+
+        }
+        if (l2 != null) {
+            current.next = l2;
+        }
+        return res.next;
+    }
+
+
+    /**
+     * 递归，更简洁
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+
+}
