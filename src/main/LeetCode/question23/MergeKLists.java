2f2e8ee56c0a86fad13e65389bdc0903d0e5f441
chengliu <1397004810@qq.com>
2018-09-04 22:05:08 +0800

    [question 23] merge K个排序的链表

---
+++ src/main/LeetCode/question23/MergeKLists.java
@@ -0,0 +1,62 @@
+package main.LeetCode.question23;
+
+import java.util.*;
+
+public class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        Queue<ListNode> queue = new PriorityQueue<>(Comparator.comparing(l -> l.val));
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.add(node);
+            }
+        }
+        ListNode top;
+        while ((top = queue.poll()) != null) {
+            current.next = top;
+            current = current.next;
+            if (top.next != null) {
+                queue.add(top.next);
+            }
+        }
+        return head.next;
+    }
+
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        else if (lists.length == 1) return lists[0];
+        else return mergeKLists(Arrays.asList(lists));
+    }
+
+    private ListNode mergeKLists(List<ListNode> lists) {
+        return lists.size() == 1 ? lists.get(0)
+                : merge(
+                        mergeKLists(lists.subList(0, lists.size()/2)),
+                        mergeKLists(lists.subList(lists.size()/2, lists.size()))
+                );
+    }
+
+
+    private ListNode merge(ListNode a, ListNode b) {
+        ListNode fh = new ListNode(0), p = fh;
+        while (a != null && b != null) {
+            if (a.val < b.val) {
+                p.next = a;
+                a = a.next;
+            } else {
+                p.next = b;
+                b = b.next;
+            }
+            p = p.next;
+        }
+        p.next = a == null ? b : a;
+        return fh.next;
+    }
+
+}
