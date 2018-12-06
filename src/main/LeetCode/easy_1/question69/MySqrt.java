924a6dacd7468f4e6f7d3bc5bf12add929973bab
chengliu <1397004810@qq.com>
2018-12-06 20:22:30 +0800

    [question83] 去除重复的节点 [question88] 合并两个已排序的数组

---
+++ src/main/LeetCode/easy_1/question69/MySqrt.java
@@ -0,0 +1,40 @@
+package main.LeetCode.easy_1.question69;
+
+public class MySqrt {
+
+    public int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+           int middle = (low + high) >> 1;
+           long square = (long) middle * (long) middle;
+           if (square == x) {
+               return middle;
+           } else if (square < x) {
+               long temp = (long) (middle + 1)  * (long) (middle + 1);
+               if (temp > x) {
+                   return middle;
+               }
+               if (temp == x) {
+                   return middle+1;
+               }
+               low = middle;
+           } else {
+               long temp = (long) (middle - 1) * (long) (middle - 1);
+               if (temp <= x) {
+                   return middle-1;
+               }
+               high = middle;
+           }
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        MySqrt test = new MySqrt();
+        System.out.println(test.mySqrt(2147395599));
+    }
+
+}
