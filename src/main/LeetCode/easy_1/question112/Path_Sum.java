c74747dcd71709c258f65df9aca386815c9dee04
chengliu <1397004810@qq.com>
2019-02-02 21:24:13 +0800

    [easy] 判断二叉树有没有路径节点的和等于给定值 [middle]

---
+++ src/main/LeetCode/easy_1/question112/Path_Sum.java
@@ -0,0 +1,16 @@
+package main.LeetCode.easy_1.question112;
+
+import main.LeetCode.TreeNode;
+
+public class Path_Sum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+
+        if(root.left == null && root.right == null && sum - root.val == 0) return true;
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+
+}
