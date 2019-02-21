5f82e8526d8a717f2671f1e30bed4432f356ae4f
chengliu <1397004810@qq.com>
2019-02-21 21:16:00 +0800

    【easy】计算n以内的质数的个数（基础解）

--- src/main/LeetCode/easy_to/question204/Count_Primes.java
+++ src/main/LeetCode/easy_to/question204/Count_Primes.java
@@ -24,4 +24,19 @@ public class Count_Primes {
         return ans;
     }
 
+
+    public int countPrimes0(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+            }
+            for (int j = 2; j*i < n; j++) {
+                notPrime[j*i] = true;
+            }
+        }
+        return count;
+    }
+
 }
