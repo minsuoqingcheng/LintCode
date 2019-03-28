056dd9eb65b9f9e2d55517591283dca5069d3ed4
chengliu <1397004810@qq.com>
2019-03-28 22:49:37 +0800

    反转链表 判断一个链表是不是回文的

---
+++ src/main/random/Palindrome_Linked_List.java
@@ -0,0 +1,41 @@
+package main.random;
+
+import main.random.basic.ListNode;
+
+public class Palindrome_Linked_List {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+}
---
+++ src/main/random/Reverse_Linked_List.java
@@ -0,0 +1,18 @@
+package main.random;
+
+import main.random.basic.ListNode;
+
+public class Reverse_Linked_List {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+}
