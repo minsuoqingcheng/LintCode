3c02356e165dc69c8c6533f8e00108bc94f6027f
chengliu <1397004810@qq.com>
2019-04-07 15:36:49 +0800

    反转链表、递归的方法

--- src/main/datastruct/Reverse_Linked_List.java
+++ src/main/datastruct/Reverse_Linked_List.java
@@ -15,4 +15,14 @@ public class Reverse_Linked_List {
         return pre;
     }
 
+
+    public ListNode reverseListI(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseListI(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
 }
