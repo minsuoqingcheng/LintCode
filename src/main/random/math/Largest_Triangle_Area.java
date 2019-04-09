8da62e7e09799c82951c7159c80f2f04d8d921eb
chengliu <1397004810@qq.com>
2019-04-09 23:22:29 +0800

    求几个点能围成的三角形的最大面积（暴力求解法，需要知道根据3个点求出面积的方法）

---
+++ src/main/random/math/Largest_Triangle_Area.java
@@ -0,0 +1,24 @@
+package main.random.math;
+
+public class Largest_Triangle_Area {
+
+    public double largestTriangleArea(int[][] points) {
+        int N = points.length;
+        double max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                for (int k = j+1; k < N; k++) {
+                    max = Math.max(max, area(points[i], points[j], points[k]));
+                }
+            }
+        }
+        return max;
+    }
+
+
+    private double area(int[] p, int[] q, int[] r) {
+        return 0.5 * Math.abs(p[0]*q[1] + q[0]*r[1] + r[0]*p[1]
+                        - q[0]*p[1] - r[0]*q[1] - p[0]*r[1]);
+    }
+
+}
