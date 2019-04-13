ae0695c0ac532ea689f17a77b64094b6c5bbc070
chengliu <1397004810@qq.com>
2019-04-13 22:52:55 +0800

    判断数组中连续的元素能够组成的最大值 在一个旋转了的数组中找出给定元素

---
+++ src/main/random/binarysearch/Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,38 @@
+package main.random.binarysearch;
+
+public class Search_in_Rotated_Sorted_Array {
+
+    /**
+     * 二分查找，找到中间值，然后判断查左还是查右
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            if (nums[left] <= nums[middle]) {
+                if (target < nums[middle] && target >= nums[left]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else {
+                if (target > nums[middle] && target < nums[left]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
