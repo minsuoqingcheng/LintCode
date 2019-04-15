6e378f43ca04d059877c041484ab3c8d6c18d704
chengliu <1397004810@qq.com>
2019-04-15 22:42:48 +0800

    判断链表的中间位置 找出数组的最长增长子序列

---
+++ src/main/random/dp/Longest_Increasing_Subsequence.java
@@ -0,0 +1,46 @@
+package main.random.dp;
+
+import java.util.Arrays;
+
+public class Longest_Increasing_Subsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int ret = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int subMax = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    subMax = Math.max(subMax, dp[j]);
+                }
+            }
+            dp[i] = subMax + 1;
+            ret = Math.max(ret, dp[i]);
+        }
+        return ret;
+    }
+
+
+    public int lengthOfLIS2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int length = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, length, num);
+            if (i < 0) {
+                i = -(i+1);
+            }
+            dp[i] = num;
+            if (i == length) {
+                length++;
+            }
+        }
+        return length;
+    }
+}
---
+++ src/main/random/linkedlist/Middle_of_the_Linked_List.java
@@ -0,0 +1,17 @@
+package main.random.linkedlist;
+
+import main.random.basic.ListNode;
+
+public class Middle_of_the_Linked_List {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
