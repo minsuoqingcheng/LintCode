a8f3e92d2062c3a73fa86282367244f522331ae5
chengliu <1397004810@qq.com>
2019-05-05 22:35:06 +0800

    抢劫房屋

---
+++ src/main/classify/dp/House_Robber.java
@@ -0,0 +1,19 @@
+package main.classify.dp;
+
+public class House_Robber {
+
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int val = nums[i];
+            dp[i+1] = Math.max(dp[i], dp[i-1]+val);
+        }
+        return dp[nums.length];
+    }
+
+}
