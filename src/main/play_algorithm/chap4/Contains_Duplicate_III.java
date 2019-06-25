d95da3c214a339eef0fbe9d6395f9bb8515aca6a
chengliu <1397004810@qq.com>
2019-06-25 23:40:53 +0800

    判断数组中有没有在间隔为K的空间内元素相同 判断数组中有没有在间隔为K且元素之间的差小于T的两个元素「TreeSet的使用，ceil和floor」

---
+++ src/main/play_algorithm/chap4/Contains_Duplicate_III.java
@@ -0,0 +1,35 @@
+package main.play_algorithm.chap4;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class Contains_Duplicate_III {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Long> records = new TreeSet<>(Comparator.naturalOrder());
+        for (int i = 0; i < nums.length; i++) {
+
+            Long floor = records.floor((long)nums[i] + (long) t);
+            Long ceil = records.ceiling((long) nums[i] - (long) t);
+
+            if (floor != null && floor >= nums[i] || ceil != null && ceil <= nums[i]) {
+                return true;
+            }
+
+
+            records.add((long) nums[i]);
+            if (records.size() == k+1) {
+                records.remove((long)nums[i-k]);
+            }
+        }
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Contains_Duplicate_III test = new Contains_Duplicate_III();
+        System.out.println(test.containsNearbyAlmostDuplicate(new int[]{4, 1, 6, 3}, 100, 1));
+    }
+}
