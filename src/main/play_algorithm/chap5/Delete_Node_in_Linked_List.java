3084ea40a680bef857ad80bd396b10330b574dc4
chengliu <1397004810@qq.com>
2019-07-07 23:02:46 +0800

    链表练习 栈练习

---
+++ src/main/play_algorithm/chap5/Delete_Node_in_Linked_List.java
@@ -0,0 +1,20 @@
+package main.play_algorithm.chap5;
+
+import main.LeetCode.ListNode;
+
+public class Delete_Node_in_Linked_List {
+
+    public void deleteNode(ListNode node) {
+
+        if (node == null) {
+            return;
+        }
+        if (node.next == null) {
+            node = null;
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
---
+++ src/main/play_algorithm/chap5/Swap_Nodes_in_Pairs.java
@@ -0,0 +1,28 @@
+package main.play_algorithm.chap5;
+
+import main.LeetCode.ListNode;
+
+public class Swap_Nodes_in_Pairs {
+
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode p = dummy;
+        while (p.next != null && p.next.next != null) {
+            ListNode p1 = p.next;
+            ListNode p2 = p1.next;
+            ListNode next = p2.next;
+
+            p2.next = p1;
+            p1.next = next;
+            p.next = p2;
+
+            p = p1;
+        }
+
+        return dummy.next;
+    }
+
+}
