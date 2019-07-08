4c141ffabbda66b797ee1d375d4d1f2a917b3473
chengliu <1397004810@qq.com>
2019-07-08 23:01:56 +0800

    二叉树的前序、后序、中序遍历非递归的实现方式 二叉树的后序遍历递归实现方式

---
+++ src/main/play_algorithm/chap6/Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,67 @@
+package main.play_algorithm.chap6;
+
+import main.LeetCode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class Binary_Tree_Inorder_Traversal {
+
+    private static final String GET = "get";
+    private static final String VISIT = "visit";
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> result = new ArrayList<>();
+        Stack<Command> stack = new Stack<>();
+        stack.push(new Command(VISIT, root));
+        while (!stack.isEmpty()) {
+            Command command = stack.pop();
+            TreeNode node = command.node;
+            if (GET.equals(command.operation)) {
+                result.add(node.val);
+            } else {
+                if (node.right != null) {
+                    stack.push(new Command(VISIT, node.right));
+                }
+                stack.push(new Command(GET, node));
+                if (node.left != null) {
+                    stack.push(new Command(VISIT, node.left));
+                }
+            }
+        }
+        return result;
+    }
+
+
+    private static class Command {
+        private String operation;
+        private TreeNode node;
+
+        Command(String operation, TreeNode node) {
+            this.operation = operation;
+            this.node = node;
+        }
+
+        public String getOperation() {
+            return operation;
+        }
+
+        public void setOperation(String operation) {
+            this.operation = operation;
+        }
+
+        public TreeNode getNode() {
+            return node;
+        }
+
+        public void setNode(TreeNode node) {
+            this.node = node;
+        }
+    }
+
+}
