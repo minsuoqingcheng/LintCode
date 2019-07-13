d595dc68a05fa2f492d6d02af92011a4297abb25
chengliu <1397004810@qq.com>
2019-07-13 22:25:36 +0800

    给个一个数，求出构成这个数所需要的平方数(1, 4, 9, ……)的最少个数

---
+++ src/main/play_algorithm/chap6/Perfect_Squares.java
@@ -0,0 +1,55 @@
+package main.play_algorithm.chap6;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Perfect_Squares {
+
+    public int numSquares(int n) {
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(n, 0));
+
+        boolean[] visited = new boolean[n+1];
+        visited[n] = true;
+
+        while (!queue.isEmpty()) {
+
+            Pair head = queue.poll();
+            int num = head.first;
+            int step = head.second;
+
+            for (int i = 1; ; i++) {
+                int left = num - i * i;
+                //已经达到要求
+                if (left == 0) {
+                    return step + 1;
+                }
+                if (left < 0) {
+                    break;
+                }
+                if (!visited[left]) {
+                    queue.add(new Pair(left, step+1));
+                    visited[left] = true;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+
+
+    static class Pair {
+
+        private int first;
+        private int second;
+
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+    }
+
+}
