a5c866fa932961710e4c9d1646e4826357379330
chengliu <1397004810@qq.com>
2019-07-14 17:06:07 +0800

    反转二叉树

---
+++ src/main/play_algorithm/chap7/Invert_Binary_Tree.java
@@ -0,0 +1,22 @@
+package main.play_algorithm.chap7;
+
+import main.LeetCode.TreeNode;
+
+public class Invert_Binary_Tree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+
+        //swap
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+
+}
