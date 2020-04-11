d19864a0c209347645fa55b473e8b16afb51f40a
liucheng <liucheng@superid.cn>
2020-04-11 22:38:29 +0800

    左旋转字符串

---
+++ src/main/classify/string/ReverseLeftWords.java
@@ -0,0 +1,9 @@
+package main.classify.string;
+
+public class ReverseLeftWords {
+
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n, s.length()) + s.substring(0, n);
+    }
+
+}
