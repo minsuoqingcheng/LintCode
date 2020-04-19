cecebaadad047fade80238e5da4c92abcb9a70fb
liucheng <liucheng@superid.cn>
2020-04-19 14:47:44 +0800

    统计字符串S1能够重复字符串S2的最大个数

---
+++ src/main/classify/simple/GetMaxRepetitions.java
@@ -0,0 +1,44 @@
+package main.classify.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetMaxRepetitions {
+
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        if (n1 == 0) {
+            return 0;
+        }
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        int l1 = c1.length;
+        int l2 = c2.length;
+        int counts1 = 0;
+        int counts2 = 0;
+        int p = 0;
+        Map<Integer, int[]> mp = new HashMap<>();
+        while (counts1 < n1) {
+            for (int i = 0; i < l1; i++) {
+                if (c1[i] == c2[p]) {
+                    p++;
+                    if (p == l2) {
+                        p = 0;
+                        counts2++;
+                    }
+                }
+            }
+            counts1++;
+            if (!mp.containsKey(p)) {
+                mp.put(p, new int[]{counts1, counts2});
+            } else {
+                int[] last = mp.get(p);
+                int circle1 = counts1 - last[0];
+                int circle2 = counts2 - last[1];
+                counts2 += circle2 * ((n1 - counts1) / circle1);
+                counts1 += circle1 * ((n1- counts1) / circle1);
+            }
+        }
+        return counts2 / n2;
+    }
+
+}
