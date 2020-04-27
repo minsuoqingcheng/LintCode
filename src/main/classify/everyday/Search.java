5dfbefb6f0fe3b9ed2fc8dc08cedfe3a5784e3cf
liucheng <liucheng@superid.cn>
2020-04-27 23:01:45 +0800

    先升序后降序的数组 二分查找 add annotation

--- src/main/classify/everyday/Search.java
+++ src/main/classify/everyday/Search.java
@@ -1,5 +1,8 @@
 package main.classify.everyday;
 
+/**
+ * 数组被切分成两部分，肯定一部分有序，一部分无序！！！
+ */
 public class Search {
 
 	public int search(int[] nums, int target) {
@@ -12,16 +15,20 @@ public class Search {
 				return middle;
 			}
 
+			//有序部分
 			if (nums[left] <= nums[middle]) {
 				if (target < nums[middle] && target >= nums[left]) {
 					right = middle - 1;
 				} else {
 					left = middle + 1;
 				}
+			//无序部分
 			} else {
+				//先升后降
 				if (target > nums[middle] && target < nums[left]) {
 					left = middle + 1;
 				} else {
+					// 升序部分
 					right = middle - 1;
 				}
 			}
