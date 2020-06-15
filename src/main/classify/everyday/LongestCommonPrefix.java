85833788b764471c3a462efcc3418279399a5910
liucheng <liucheng@superid.cn>
2020-06-15 23:03:38 +0800

    编写一个函数来查找字符串数组中的最长公共前缀。

---
+++ src/main/classify/everyday/LongestCommonPrefix.java
@@ -0,0 +1,36 @@
+package main.classify.everyday;
+
+public class LongestCommonPrefix {
+
+	public String longestCommonPrefix(String[] strs) {
+		if (strs.length < 1) {
+			return "";
+		}
+		if (strs.length == 1) {
+			return strs[0];
+		}
+		String first = strs[0];
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < first.length(); i++) {
+			boolean append = true;
+			for (int j = 1; j < strs.length; j++) {
+				if (i >= strs[j].length() || i < strs[j].length() && first.charAt(i) != strs[j].charAt(i)) {
+					append = false;
+					break;
+				}
+			}
+			if (append && i == builder.length()) {
+				builder.append(first.charAt(i));
+			}
+
+		}
+		return builder.toString();
+
+	}
+
+	public static void main(String[] args) {
+		LongestCommonPrefix test = new LongestCommonPrefix();
+
+	}
+
+}
