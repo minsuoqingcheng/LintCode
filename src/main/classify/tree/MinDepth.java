e00895ae8a5878348d1fc42b5fef9b02f83eee34
chengliu <1397004810@qq.com>
2021-01-11 22:22:34 +0800

    树的最小深度； 生成括号

---
+++ src/main/classify/tree/MinDepth.java
@@ -0,0 +1,20 @@
+package main.classify.tree;
+
+import main.classify.basic.TreeNode;
+
+public class MinDepth {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+}
